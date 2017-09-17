ab6e7a66500f3a2cc57a6f771139299828f9bfd4
xianmingli <xmli@stanford.edu>
2017-09-17 11:42:15 -0700

    Implemented sort!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { EventbriteService } from "./services/eventbrite.service";
 import { EventbriteOrderInterface } from "./interfaces/eventbrite-order.interface";
 import { EventbriteVenueInterface } from "./interfaces/eventbrite-venue.interface";
 import { IcalendarInfoInterface } from "./interfaces/icalendar-info.interface";
+import { EventbriteEventInterface } from "./interfaces/eventbrite-event.interface";
 
 import {DataSource} from '@angular/cdk/collections';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/forkJoin'
 
 @Component({
   selector: 'app-root',
@@ -17,8 +19,10 @@ import 'rxjs/add/observable/of';
 })
 export class AppComponent implements OnInit{
 
+  sort_NewToOld = true;
   my_orders:EventbriteOrderInterface[];
-  my_events:IcalendarInfoInterface[] = [];
+  my_calevents:IcalendarInfoInterface[] = [];
+  my_events = [{}];
 
   constructor(private _eventbriteService: EventbriteService) { }
   
@@ -39,46 +43,21 @@ export class AppComponent implements OnInit{
         }
       },
       () => { //complete
-        console.log("Data loaded.")
         this.createDownloadOptions(this.my_orders);
       }
     );    
   }
 
-  createDownloadOptions(orders:EventbriteOrderInterface[]) {
-    orders.forEach(element => {
-      var ev_id = element.event_id;
-      this._eventbriteService.getEventbriteEvent(ev_id)
-      .subscribe(
-        data => {
-          if(data.venue_id !== null) {
-            this.buildEvent(data.url, data.start.utc, data.end.utc, data.name.text, data.description.text, data.venue_id);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          if (err.error instanceof Error) {
-            console.log('An error occurred:', err.error.message);
-          } else {
-            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-          }
-        }
-      );
+  createDownloadOptions(orders:EventbriteOrderInterface[]) {  
+    let observableBatch = [];    
+    orders.forEach(( element ) => {
+      observableBatch.push( this._eventbriteService.getEventbriteEvent(element.event_id) );
     });
-    console.log("EVENTS:");
-    console.log(this.my_events);
-  }
 
-  buildEvent(url, raw_dtstart, raw_dtend, summary, desc, venue_id){
-    var loc;
-    this._eventbriteService.getVenueLocation(venue_id)
+    var subs = Observable.forkJoin(observableBatch)
     .subscribe(
       data => {
-        if(data.name !== null) {
-          loc = data.name;
-          if(data.address.localized_address_display !== null) {
-            loc += ', ' + data.address.localized_address_display;     
-          }     
-        }
+        this.my_events = data;
       },
       (err: HttpErrorResponse) => {
         if (err.error instanceof Error) {
@@ -88,30 +67,110 @@ export class AppComponent implements OnInit{
         }
       },
       () => {
-        var file_begin = "BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT";
-        var file_end = "END:VEVENT\nEND:VCALENDAR";    
-        
-        var text = file_begin+"\n" 
-        + "URL:" + url + "\n" 
-        + "DTSTART:" + this.parseDate(raw_dtstart) + "\n" 
-        + "DTEND:" + this.parseDate(raw_dtend)+"\n" 
-        + "SUMMARY:" + summary+"\n" 
-        + "LOCATION:"
-
-        if(loc == null) loc = "";
-        text += loc + '\n' 
-        + "DESCRIPTION:" + desc + "\n"                
-        + file_end;
-        this.my_events.push({
-          filename:summary+".ics",
-          eventname:summary,
-          eventdate:raw_dtstart,
-          calbody:text,
-          description:desc,
-          url:url
+        let observableBatchInner = [];    
+        this.my_events.forEach(( elem:EventbriteEventInterface ) => {
+          if(elem.venue_id !== null) {
+            observableBatchInner.push( this._eventbriteService.getVenueLocation(elem.venue_id));
+          } else {
+            observableBatchInner.push( Observable.of({}));            
+          }
         });
+        var subsInner = Observable.forkJoin(observableBatchInner).subscribe(
+          data => {
+            // console.log(data);
+            for (var i = 0; i < this.my_events.length; i++) {
+              var ev = this.my_events[i];
+              var loc = data[i];
+              
+              this.buildCalendarEvent(ev, loc);
+              
+            }
+          },
+          (err: HttpErrorResponse) => {
+            if (err.error instanceof Error) {
+              console.log('An error occurred:', err.error.message);
+            } else {
+              console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+            }
+          },
+          () => {
+            this.my_calevents.sort(this.sortNewToOld);
+            console.log("Data loaded.")    
+          } 
+        );            
       }
     );
+  }
+
+  sortUpdate() {
+    if(this.sort_NewToOld) {
+      this.my_calevents.sort(this.sortOldToNew);      
+    } else {
+      this.my_calevents.sort(this.sortNewToOld);      
+    }
+    this.sort_NewToOld = !this.sort_NewToOld;
+  }
+
+  sortOldToNew(a,b) {
+    var dateA = new Date(a.eventdate).getTime();
+    var dateB = new Date(b.eventdate).getTime();
+    return dateA > dateB ? 1 : -1;  
+  }
+
+  sortNewToOld(a,b) {
+    var dateA = new Date(a.eventdate).getTime();
+    var dateB = new Date(b.eventdate).getTime();
+    return dateA > dateB ? -1 : 1;  
+  }
+
+  buildCalendarEvent(ev, loc){    
+    var event:EventbriteEventInterface = ev;
+    var location:EventbriteVenueInterface = loc;
+
+    var file_begin = "BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT";
+    var file_end = "END:VEVENT\nEND:VCALENDAR";    
+    
+    var text = file_begin+"\n" 
+    + "URL:" + event.url + "\n" 
+    + "DTSTART:" + this.parseDate(event.start.utc) + "\n" 
+    + "DTEND:" + this.parseDate(event.end.utc)+"\n" 
+    + "SUMMARY:" + event.name.text+"\n" 
+    + "LOCATION:"
+
+    if(this.isEmpty(location)){
+      text += '\n'
+    } else {
+      if(location.name !== null) {
+        text += location.name;  
+        if(location.address.localized_address_display !== null) {
+          text += ' - ' + location.address.localized_address_display + '\n';            
+        }   
+      } else {
+        if(location.address.localized_address_display !== null) {
+          text += location.address.localized_address_display + '\n';            
+        } 
+      }
+    }
+
+    text += "DESCRIPTION:" + event.description.text + "\n"                
+    + file_end;
+    
+    this.my_calevents.push({
+      filename:event.name.text+".ics",
+      eventname:event.name.text,
+      eventdate:event.start.utc,
+      calbody:text,
+      description:event.description.text,
+      url:event.url
+    });
+  }
+
+  isEmpty(obj) {
+    for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+        return false;
+    }
+    return true;
   }
 
   downloadCalendar(event:IcalendarInfoInterface) {
